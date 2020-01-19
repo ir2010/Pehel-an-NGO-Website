c2699cc5c269192f9264a372aff7ee893fa28d55
ir2010 <46022116+ir2010@users.noreply.github.com>
2020-01-19 17:01:22 +0530

    Add files via upload

---
+++ Nayi Dishayein.php
@@ -0,0 +1,429 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="Slideshow_large.css">
+<link rel="stylesheet" href="Slideshow_small.css">
+<link rel="stylesheet" href="Tabs.css">
+<link rel="stylesheet" href="Headings.css">
+<link rel="stylesheet" href="Sidenav.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<style>
+
+body{
+      background-image: url("Background-opera-speeddials-community-web-simple-backgrounds.jpg");
+	  background-size: cover;
+	  top-margin: 0;
+	  overflow-x: hidden;
+	}
+	
+.button{
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+      }
+	  
+.button:hover{
+           background-color: grey;
+		   border-color: grey;
+		  }
+	
+</style>
+<title> Pehel - Ek Kadam </title>
+</head>
+<!--
+<div id="sidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Home</a>
+  <a href="#">About Us</a>
+  <a href="#">Current Issues</a>
+  <a href="#">Tied-up NGOs</a>
+  <a href="#">Donate</a>
+  <a href="#">Contact Us</a>
+</div>
+<br>
+
+<div class="icon" onclick="openNav()"></div>
+<div class="icon" onclick="openNav()"></div>
+<div class="icon" onclick="openNav()"></div>
+
+-->
+
+
+<body>
+
+<div class="header">
+<h1>Title </h1>
+<h3> Motto...........................</h3>
+<br>
+</div>
+
+<div class="tabs">
+  <a href="#">Home</a>
+  <a href="#About">About Us</a>
+  <a href="#Cur_Iss">Current Issues</a>
+  <a href="NGOs.html" target="_blank">Tied-up NGOs</a>
+  <a href="#Donate">Donate</a>
+  <a href="#Contact">Contact Us</a>
+</div>
+
+<div class="slideshow_large">
+   <div class="slide_large fade">
+   <img src="poor.jpg" style="width:100vw; height:550px; padding-top:15px; overflow:hidden;">
+   <div class="text">Children driving themselves out of poverty.</div>
+   </div>
+   
+   <div class="slide_large fade">
+   <img src="edu.jpg" style="width:100vw; height:550px; padding-top:15px; overflow:hidden;">
+   <div class="text">Enlightened souls will brighten the world.</div>
+   </div>
+   
+   <div class="slide_large fade">
+   <img src="labour.jpg" style="width:100vw; height:550px; padding-top:15px; overflow:hidden;">
+   <div class="text">The tender shoulders are for carrying school bags not household responsibilities.</div>
+   </div>
+   
+   <div class="slide_large fade">
+   <img src="rape.jpg" style="width:100vw; height:550px; padding-top:15px; overflow:hidden;">
+   <div class="text">"And, if everyone lit just one little candle, what a bright world it would be."</div>
+   </div>
+   
+   <div class="slide_large fade">
+   <img src="med.jpeg" style="width:100vw; height:550px; padding-top:15px; overflow:hidden;">
+   <div class="text" style="color: black;">It takes small efforts to bring about a big difference.</div>
+   </div>
+   
+   <div style="text-align:center">
+      <span class="dot" onclick="currentSlide(1)"></span> 
+      <span class="dot" onclick="currentSlide(2)"></span> 
+      <span class="dot" onclick="currentSlide(3)"></span> 
+      <span class="dot" onclick="currentSlide(4)"></span> 
+      <span class="dot" onclick="currentSlide(5)"></span> 
+   </div>
+   
+   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+   <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<a name="About">
+<br>
+<br>
+
+<hr>
+
+
+
+<br>
+<br>
+
+<div class="heading">
+   <h2>About Us</h2>
+</div>
+
+<p style="font-family:Arial; font-size:15px;"> cccvfdggfhgfgfssa xcsdrtry5676vdffryytrsxae455gfvcx xder<br>dsfjsdfuhjfuerhfurejfie<br>w
+ejhdwehdsdjcnwedwe<br>jnfsjdhdwuhwueuei</p>
+
+
+<a name="Cur_Iss">
+<br>
+<br>
+
+<hr>
+
+<br>
+<br>
+<br>
+
+
+<div class="heading">
+   <h2>Current Issues</h2>
+</div>
+
+<div class="Cur1">
+
+<h3>i) Children death toll rises to 160+ in Muzaffarpur</h3>
+
+<br>
+
+<div class="slideshow_small">
+   <div class="slide_small 1 fade">
+   <img src="poor.jpg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small">Children driving themselves out of poverty.</div>
+   </div>
+   
+   <div class="slide_small 1 fade">
+   <img src="edu.jpg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small">Enlightened souls will brighten the world.</div>
+   </div>
+   
+   <div class="slide_small 1 fade">
+   <img src="labour.jpg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small">The tender shoulders are for carrying school bags not household responsibilities.</div>
+   </div>
+   
+   <div class="slide_small 1 fade">
+   <img src="rape.jpg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small">"And, if everyone lit just one little candle, what a bright world it would be."</div>
+   </div>
+   
+   <div class="slide_small 1 fade">
+   <img src="med.jpeg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small" style="color: black;">It takes small efforts to bring about a big difference.</div>
+   </div>
+  
+</div>
+<p> &nbsp; nnjikjgvfdxrdtygbjkilkmmmjk
+jjhiujkjnbgvtyguyhikokpoiopmjnuj
+bhyhijlmkmk</p>
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<h3 style="font-size: 22px; font-family:'Times New Roman';">&nbsp; Tied-up NGOs(operating in Muzaffarpur) </h3>
+
+<div style="padding-left: 20px;">
+<br>
+<h4 style="font-size: 20px;"> i) Mahila Development Centre </h4>
+
+<p style="overflow:auto;"> &nbsp; &nbsp;Mahila development center is a non-government organization. A Sarvodaya activist Mr. Paramhans Prasad Singh founded it. The work of MDC is in districts of north Bihar. Mr. Singh is associated with Sarvodaya movement under the leadership of Loknayak Jai Prakash Narayan in Bihar. His contribution was also in the anti naxal movement in Mushahri block of Muzaffarpur district. He worked with down trodden and landless farmers. His contribution in the field of rural development is commendable. 
+In late eighties he concentrated his work in Muzaffarpur and founded Mahila Development Center.</p>
+
+<br><br>
+
+<a href="http://www.mahiladevelopmentcentre.org/" target="_blank">Click here</a> to visit them.
+<br><br><br>
+<a href="NGOs.html#mdc1" target="_blank">Click here</a> to view the assurance letter.
+<br>
+<br>
+<br>
+<div style="font-size:25px; font-family:Arial;"> Trust Us <i class='far fa-smile'></i> &nbsp; &nbsp; &nbsp; &nbsp; 
+<a href="#" class="button">Donate</a>
+</div>
+</div>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+
+<div class="Cur2" style="float:left;">
+
+<h3>ii) Children death toll rises to 160+ in Muzaffarpur</h3>
+
+<br>
+
+<div class="slideshow_small"> 
+   <div class="slide_small 2 fade">
+   <img src="poor.jpg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small">Children driving themselves out of poverty.</div>
+   </div>
+   
+   <div class="slide_small 2 fade">
+   <img src="edu.jpg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small">Enlightened souls will brighten the world.</div>
+   </div>
+   
+   <div class="slide_small 2 fade">
+   <img src="labour.jpg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small">The tender shoulders are for carrying school bags not household responsibilities.</div>
+   </div>
+   
+   <div class="slide_small 2 fade">
+   <img src="rape.jpg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small">"And, if everyone lit just one little candle, what a bright world it would be."</div>
+   </div>
+   
+   <div class="slide_small 2 fade">
+   <img src="med.jpeg" style="width:20vw; height:200px; padding-top:15px; overflow:hidden;">
+   <div class="text_small" style="color: black;">It takes small efforts to bring about a big difference.</div>
+   </div>
+  
+</div>
+
+<p> &nbsp; nnjikjgvfdxrdtygbjkilkmmmjk
+jjhiujkjnbgvtyguyhikokpoiopmjnuj
+bhyhijlmkmk</p>
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<h3 style="font-size: 22px; font-family:'Times New Roman';">&nbsp; Tied-up NGOs(operating in Muzaffarpur) </h3>
+
+<div style="padding-left: 20px;">
+<br>
+
+<h4 style="font-size: 20px;"> i) Mahila Development Centre </h4>
+
+<p style="overflow:auto;"> &nbsp; &nbsp;Mahila development center is a non-government organization. A Sarvodaya activist Mr. Paramhans Prasad Singh founded it. The work of MDC is in districts of north Bihar. Mr. Singh is associated with Sarvodaya movement under the leadership of Loknayak Jai Prakash Narayan in Bihar. His contribution was also in the anti naxal movement in Mushahri block of Muzaffarpur district. He worked with down trodden and landless farmers. His contribution in the field of rural development is commendable. 
+In late eighties he concentrated his work in Muzaffarpur and founded Mahila Development Center.</p>
+
+<br><br>
+
+<a href="http://www.mahiladevelopmentcentre.org/" target="_blank">Click here</a> to visit them.
+<br><br><br>
+<a href="NGOs.html#mdc2" target="_blank">Click here</a> to view the assurance letter.
+<br>
+<br>
+<br>
+<div style="font-size:25px; font-family:Arial;"> Trust Us <i class='far fa-smile'></i> &nbsp; &nbsp; &nbsp; &nbsp; 
+<a href="#" class="button">Donate</a>
+</div>
+</div>
+
+</div>
+
+<br>
+<br>
+<br>
+
+<h5 style="font-size:20px; font-family:Arial; font-style:italics; margin-top: 10px;"> We'll be taking up many other issues soon.</h5>
+ 
+<br>
+<br>
+
+<hr>
+
+<a name="Donate">
+<br>
+<br>
+
+<div class="heading">
+   <h2>Donate</h2>
+</div>
+
+<br>
+
+<p> We'd be using the funds we collect in various constructive works in the society. <br>To contribute to our fund:&nbsp; &nbsp; &nbsp;
+<a href="#" class="button">Click Here</a>
+<a name="Contact">
+
+<br>
+<br>
+<hr>
+<br>
+<br>
+
+
+<div id="contact" style="background-color:#808080; padding-top:20px; padding-left:10px;padding-bottom:30px;">
+
+<div class="heading">
+   <h2>Contact Us</h2>
+</div>
+
+<i class="fas fa-phone"></i> &nbsp; 987765578989
+<br> <br>
+<i class="material-icons">email</i> &nbsp; pehelekkadam@gmail.com
+
+</div>
+</div>
+
+<div style="background-color:#a6a6a6; height:35px; padding-top: 20px; padding-bottom:20px; padding-left: 20px; text-align: center; font-weight: bold; font-size: 20px; font-family: Verdana;">
+Keep up the spirit!&nbsp; 
+Thanks for visiting us! <i class='far fa-smile'></i>
+<br>
+<br>
+<h5 style="font-size:8px;"> Designed by IR &nbsp; <i class='far fa-copyright'></i>
+</div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+showSlides_small_1();
+showSlides_small_2();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide_large");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 3 seconds
+}
+
+function showSlides_small_1() {
+  var i;
+  var slides = document.getElementsByClassName("slide_small 1");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides_small_1, 5000); // Change image every 3 seconds
+}
+
+function showSlides_small_2() {
+  var i;
+  var slides = document.getElementsByClassName("slide_small 2");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides_small_2, 5000); // Change image every 3 seconds
+}
+
+function plusSlides(n) {
+  showSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide_large");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+</body>
+</html>
